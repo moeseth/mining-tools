95c4a444c438d0f754b6320f66b3a1e321c9332b
moeseth <slamduck@gmail.com>
2018-01-16 01:25:59 +0630

    Update remove_ether_devfee.py

--- remove_ether_devfee.py
+++ remove_ether_devfee.py
@@ -1,7 +1,6 @@
 # based on:
 # https://stackoverflow.com/questions/27293924/change-tcp-payload-with-nfqueue-scapy?rq=1
 # https://github.com/DanMcInerney/cookiejack/blob/master/cookiejack.py
-## remove claymore ether and altcoins devfee
 
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
@@ -17,8 +16,7 @@ from datetime import datetime, timedelta
 
 # https://forum.z.cash/t/about-dev-fees-and-how-to-remove-them/9600/36
 ## hook all the outgoing connections to replace our address
-os.system('iptables -t nat -A OUTPUT -p tcp --match multiport --dports 3333,14444,4444,9999,5000,5005,8008,20535,20536,20537 -j DNAT --to-destination 139.99.9.157:4444')  # for dwarfp
-os.system('iptables -A OUTPUT -p tcp --match multiport --dports 3333,14444,4444,9999,5000,5005,8008,20535,20536,20537 -j NFQUEUE --queue-num 0')  # for dwarfpool
+os.system('iptables -A OUTPUT -p tcp --dport 4444 -j NFQUEUE --queue-num 0')
 
 my_eth_address = '0x0f4f79bdfbb6a3540f7379cf0f708d55c2b1b35d'
 
@@ -39,7 +37,7 @@ def callback(arg1, payload):
         print('[*] DevFee Detected - Replacing Address - %s\n' % datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
         print('[*] REPLACED FROM %s TO %s\n' % (json_data['params'][0], my_eth_address))
 
-        json_data['params'][0] = "%s.devfee" % (my_eth_address)
+        json_data['params'][0] = my_eth_address
 
         print("[*] AFTER: %s\n" % json.dumps(json_data))
 
